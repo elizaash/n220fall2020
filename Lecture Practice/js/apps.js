3a9cf930b52cac7e2ecb362376dbb03dbb5dc787
elizaash <elizaash@users.noreply.github.com>
2020-10-16 19:09:21 -0400

    Midterm Review

--- Lecture Practice/js/apps.js
+++ Lecture Practice/js/apps.js
@@ -1,37 +1,98 @@
+//___________________________________________________________________
+// FUNCTIONS LECTURE 
 
-let sunX = 40;
+    // let sunX = 40;
 
-function setup() {
-    createCansvas(400,300);
-    background(100);
+    // function setup() {
+    //     createCansvas(400,300);
+    //     background(100);
 
-    
-}
-
-function draw(){
-    
-    background(100);
-    sunX ++;
-    drawSun("#00FF00", 15, mouseY, mouseX );
-    
-}
-
-function drawSun(sunColor, numRays, yHeight, xHeight){
-
-    fill(sunColor);
-    stroke(sunColor);
-
-    for(var i = 0; i < numRays; i++){
-        line(xHeight, yHeight, i*20, 300);
         
-        circle(xHeight, yHeight, 40);
+    // }
+
+    // function draw(){
+        
+    //     background(100);
+    //     sunX ++;
+    //     drawSun("#00FF00", 15, mouseY, mouseX );
+        
+    // }
+
+    // function drawSun(sunColor, numRays, yHeight, xHeight){
+
+    //     fill(sunColor);
+    //     stroke(sunColor);
+
+    //     for(var i = 0; i < numRays; i++){
+    //         line(xHeight, yHeight, i*20, 300);
+            
+    //         circle(xHeight, yHeight, 40);
+    //     }
+    // }
+
+
+    // console.log("Hello outside");
+
+    // function sayHello(){
+    //     console.log("Hello inside");
+    // }
+    // sayHello();
+//___________________________________________________________________
+
+//OBJECTS LECTURE & MIDTERM
+
+    //OBJECT 1 : TRAILING CIRCLE
+    // var myCircle = { 
+    //     r: 15,
+    //     x: 20,
+    //     y: 30,
+    //     color: [199, 137, 80],
+    //     speed:.2,
+    //     update: function () {
+    //         fill(this.color);
+    //         this.x += this.speed;
+    //         circle(this.x, this.y, this.r);
+    //     }
+    // }
+    // function setup(){
+    //     createCanvas(400,300);
+    
+    // }
+    
+    // function draw() {
+        
+    //     myCircle.update();
+    // }
+
+    //OBJECT 2 : DRIPPING WATER
+
+    var circles = [
+        {x: 100, y: 20, r: 5},
+        {x: 20, y: 50, r: 50},
+        {x: 150, y: 200, r: 25},
+        {x: 300, y: 250, r: 100},
+    ];
+
+    function setup(){
+        createCanvas(400,300);
+        noStroke();
+        fill(200);
     }
-}
 
+    function draw() {
+        for (var i = 0; i < circles.length; i++) {
+            let currentCircle = circles[i];
+            circle(currentCircle.x,currentCircle.y,currentCircle.r);
+            //drip
+            currentCircle.y += 2;
 
-console.log("Hello outside");
+            if(currentCircle.y > 300) {
+                currentCircle.y = 0;
+                currentCircle.x = Math.random() * 400;
+                fill(Math.random() * 255)
+            }
 
-function sayHello(){
-    console.log("Hello inside");
-}
-sayHello();
+        }  
+       
+    }
+
---
+++ Midterm/review assignment/js/apps.js
@@ -0,0 +1,145 @@
+function setup() {
+    createCanvas(800, 600)
+}
+
+//variables
+    let circleX = 350;
+    let circleY = 275;
+
+    let circleXSpeed = -3;
+    let circleYSpeed = 3;
+
+    let rectX = 750;
+    let rectY = 0;
+    rectW = 20;
+    rectH = 150; 
+
+    let rectXSpeed = 0;
+    let rectYSpeed = 2;
+
+    
+
+
+//shapes
+
+    function draw(){
+
+        background('#8e9185');
+
+        //circle
+            circleX = circleX + circleXSpeed;
+            circleY = circleY + circleYSpeed;
+                noStroke();
+                circle(circleX, circleY, 20);
+                
+            
+                    
+        //rectangle
+            rectX = rectX + rectXSpeed;
+            rectY = rectY + rectYSpeed;
+                //rectangle sscreen bounce    
+                    if (rectY > height-150 ) {
+                        rectYSpeed = -2;
+                        }
+            
+                    if (rectY < 0) {
+                        rectYSpeed = 2;
+                        }   
+                        
+                    rect(rectX, rectY, rectW, rectH);
+        
+//collison code
+
+        function collideRect(circleX, circleY, rectX, rectY, rectW, rectH) {
+
+                        var colliding = false;
+                
+                        if(circleX > rectX && circleX < rectX + rectW) {
+                
+                            if(circleY > rectY && circleY < rectY + rectH) {
+                
+                                return true;
+                
+                            }
+                
+                    }
+                
+                    return colliding;
+                
+                    }    
+                        result = collideRect(0, 40, 0, 550, 150, 20);   
+
+
+    //bounce off rect
+
+        if(collideRect(circleX, circleY, rectX, rectY, rectW, rectH)){
+            circleXSpeed = -4;
+            
+            }
+        
+        if(collideRect(circleX, circleY, rectX, rectY, rectW, rectH)){
+            circleYSpeed = -4;
+            
+            }
+
+    //bounce off walls
+
+        
+        if (circleY > height) {
+            circleYSpeed = -4;
+            }
+        if (circleX < 0) {
+            circleXSpeed = 4;
+            }
+
+        if (circleY < 0) {
+            circleYSpeed = 4;
+            }
+
+     //key movements
+            if(keyIsDown(UP_ARROW)) {
+                rectYSpeed = -5;
+                fill(255,211,168);
+            }
+
+            if(keyIsDown(DOWN_ARROW)) {
+                rectYSpeed = 5;
+                fill(255,255,255);
+            }
+
+
+    //ball reappear
+            if (circleX > width) {
+            circleX = 400;
+            circleY = 250;
+            circleXSpeed = -4;
+
+            }
+
+            
+
+
+
+    //text        
+            textSize(25);
+            fill(255,255,255);  
+            textFont('Impact');
+            text('PONGISH', 350, 250);
+            textAlign(CENTER, CENTER) 
+            
+            // textSize(20);
+            // fill(255,255,255); 
+            // textFont('Georgia');
+            // text('ball drop', 350, 300);
+            // textAlign(CENTER, CENTER)  
+
+            //circle(350,275,20);
+
+
+
+        
+
+                    
+        console.log(collideRect());                  
+     }
+
